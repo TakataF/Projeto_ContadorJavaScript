f8161d5da71bb643cd790211e927205db750d47e
Felipe Takata de Alvarenga <felipetakatadealvarenga@gmail.com>
2022-08-19 14:05:43 -0300

    Feat reset all clicks and insert Bootstrap

--- Projeto Contador JavaScript/script.js
+++ Projeto Contador JavaScript/script.js
@@ -10,5 +10,12 @@ function addreset(){
     reset += 1;
     document.getElementById("clicker").innerHTML=`clicks:${clicker}`;
     document.getElementById("reset").innerHTML=`Reset:${reset}`;
+}
+
+function addzero(){
+    clicker = 0;
+    reset = 0;
+    document.getElementById("clicker").innerHTML=`clicks:${clicker}`;
+    document.getElementById("reset").innerHTML=`Reset:${reset}`;
 
 }
